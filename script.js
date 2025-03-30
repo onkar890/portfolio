68eff77c35a37f7ec955e93584b71107d8eaff2f
Onkar Kumbhar <85824946+onkar890@users.noreply.github.com>
2025-03-30 17:46:44 +0530

    Update script.js

--- script.js
+++ script.js
@@ -36,18 +36,4 @@
             this.reset();
         });
 
-        document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-            anchor.addEventListener('click', function (e) {
-                e.preventDefault();
-                const target = document.querySelector(this.getAttribute('href'));
-                const headerOffset = 80;
-                const elementPosition = target.getBoundingClientRect().top;
-                const offsetPosition = elementPosition + window.pageYOffset - headerOffset;
-
-                window.scrollTo({
-                    top: offsetPosition,
-                    behavior: 'smooth'
-                });
-            });
-        });
    
